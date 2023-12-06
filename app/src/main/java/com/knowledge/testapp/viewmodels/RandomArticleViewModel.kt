bd53ce56594a4df4b72af4d595960af91bc524e1
NeedThisAccount <36775976+Retardeded@users.noreply.github.com>
2023-12-06 13:24:36 +0100

    for non-eng langs fetching random article/translating it multiple times in case given wiki lang does not have corresponding article

--- app/src/main/java/com/knowledge/testapp/viewmodels/RandomArticleViewModel.kt
+++ app/src/main/java/com/knowledge/testapp/viewmodels/RandomArticleViewModel.kt
@@ -1,12 +1,12 @@
 package com.knowledge.testapp.viewmodels
 
 import android.os.Build
+import android.util.Log
 import androidx.annotation.RequiresApi
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.google.firebase.database.*
-import com.knowledge.testapp.utils.ConstantValues
 import com.knowledge.testapp.data.Language
 import com.knowledge.testapp.utils.UserManager
 import kotlinx.coroutines.Dispatchers
@@ -20,9 +20,8 @@ import java.net.HttpURLConnection
 import java.net.URL
 import java.net.URLEncoder
 import java.util.*
+import java.util.concurrent.CountDownLatch
 import kotlin.collections.HashMap
-import kotlin.coroutines.resume
-import kotlin.coroutines.suspendCoroutine
 
 class RandomArticleViewModel : ViewModel() {
 
@@ -93,7 +92,172 @@ class RandomArticleViewModel : ViewModel() {
         })
     }
 
-    fun countTitlesPerCategory() {
+    suspend fun getRandomWikiEntryInCorrectLanguage(): String? {
+        val entries = getRandomConsecutiveWikiEntries(10)
+        entries.forEach { title ->
+            if (title != null) {
+                if (lang == Language.ENGLISH.languageCode) {
+                    return title
+                } else {
+                    val translatedTitle = translateTitleToLanguage(title, lang)
+                    if (translatedTitle != null) {
+                        if (translatedTitle.isNotEmpty()) {
+                            return translatedTitle
+                        }
+                    }
+                }
+            }
+        }
+        return null
+    }
+
+    suspend fun translateTitleToLanguage(title: String, languageCode: String): String? = withContext(Dispatchers.IO) {
+        val apiUrl = buildWikipediaApiUrl(title, languageCode)
+        try {
+            URL(apiUrl).openConnection().let { connection ->
+                (connection as HttpURLConnection).run {
+                    requestMethod = "GET"
+                    inputStream.bufferedReader().use { it.readText() }.let { response ->
+                        return@let parseTranslationFromResponse(response, languageCode)
+                    }
+                }
+            }
+        } catch (e: Exception) {
+            Log.e("TranslateTitle", "Error in translating title: ${e.localizedMessage}")
+            null
+        }
+    }
+
+    private fun buildWikipediaApiUrl(title: String, languageCode: String): String {
+        val encodedTitle = URLEncoder.encode(title, "UTF-8")
+        return "https://en.wikipedia.org/w/api.php?action=query&prop=langlinks&titles=$encodedTitle&lllang=$languageCode&format=json"
+    }
+
+    private fun parseTranslationFromResponse(response: String, languageCode: String): String? {
+        return try {
+            val json = JSONObject(response)
+            val pages = json.optJSONObject("query")?.optJSONObject("pages")
+            val firstPageId = pages?.keys()?.next()
+            val langLinks = pages?.optJSONObject(firstPageId)?.optJSONArray("langlinks")
+
+            if (langLinks != null && langLinks.length() > 0) {
+                val langLink = langLinks.optJSONObject(0)
+                if (langLink.getString("lang") == languageCode) {
+                    langLink.optString("*")
+                } else {
+                    null
+                }
+            } else {
+                null
+            }
+        } catch (e: JSONException) {
+            Log.e("ParseTranslation", "JSON parsing error: ${e.localizedMessage}")
+            null
+        }
+    }
+
+    suspend fun getRandomConsecutiveWikiEntries(entriesCount: Int): List<String?> = withContext(Dispatchers.IO) {
+        val randomStartIndex = (0 until 8000).random()
+        val query = mostPopularPagesRef.orderByKey().startAt(randomStartIndex.toString()).limitToFirst(entriesCount)
+        try {
+            val dataSnapshot = query.get().await()
+            if (dataSnapshot.exists()) {
+                return@withContext dataSnapshot.children.map { childSnapshot ->
+                    childSnapshot.child("title").value.toString()
+                }
+            }
+        } catch (e: Exception) {
+            Log.e("RandomArticleViewModel", "Error fetching entries: ${e.localizedMessage}")
+        }
+
+        return@withContext emptyList<String?>()
+    }
+    suspend fun getRandomArticleFromCategory(category: String): String? = withContext(Dispatchers.IO) {
+        try {
+            val translatedCategory = if (lang != Language.ENGLISH.languageCode) {
+                categoriesTranslation[category] ?: category
+            } else {
+                category
+            }
+
+            val query = mostPopularPagesRef.orderByChild("category").equalTo(translatedCategory)
+            val dataSnapshot = query.get().await()
+
+            if (dataSnapshot.exists()) {
+                val entries = dataSnapshot.children.mapNotNull { it.child("title").value as? String }.shuffled()
+
+                for (title in entries) {
+                    val translatedTitle = if (lang == Language.ENGLISH.languageCode) {
+                        title
+                    } else {
+                        translateTitleToLanguage(title, lang)
+                    }
+
+                    if (!translatedTitle.isNullOrEmpty()) {
+                        return@withContext translatedTitle
+                    }
+                }
+            }
+        } catch (e: Exception) {
+            Log.e("RandomArticleVM", "Error fetching article: ${e.localizedMessage}")
+        }
+        return@withContext null
+    }
+
+    suspend fun getArticleByKeyword(keyword: String): String? = withContext(Dispatchers.IO) {
+        val urlStr = buildWikipediaApiUrlKeyword(keyword, lang)
+        var urlConnection: HttpURLConnection? = null
+
+        try {
+            val url = URL(urlStr)
+            urlConnection = (url.openConnection() as HttpURLConnection).apply {
+                requestMethod = "GET"
+            }
+
+            if (urlConnection.responseCode == HttpURLConnection.HTTP_OK) {
+                urlConnection.inputStream.bufferedReader().use { reader ->
+                    val response = reader.readText()
+                    return@withContext parseRandomArticleFromKeyword(response)
+                }
+            } else {
+                Log.e("getArticleByKeyword", "HTTP error code: ${urlConnection.responseCode}")
+            }
+        } catch (e: Exception) {
+            Log.e("getArticleByKeyword", "Error fetching article by keyword: ${e.localizedMessage}")
+        } finally {
+            urlConnection?.disconnect()
+        }
+
+        null
+    }
+
+    private fun buildWikipediaApiUrlKeyword(keyword: String, languageCode: String): String {
+        val encodedKeyword = URLEncoder.encode(keyword, "UTF-8")
+        return "https://${languageCode}.wikipedia.org/w/api.php?action=query&list=search&srsearch=$encodedKeyword&format=json"
+    }
+
+    fun parseRandomArticleFromKeyword(json: String): String? {
+        return try {
+            val jsonObject = JSONObject(json)
+            val searchArray = jsonObject.optJSONObject("query")?.optJSONArray("search")
+
+            searchArray?.let {
+                val titles = List(it.length()) { index -> it.optJSONObject(index)?.optString("title") }
+                    .filterNotNull()
+
+                titles.randomOrNull()
+            }
+        } catch (e: JSONException) {
+            Log.e("ArticleParsing", "Error parsing JSON for articles: ${e.localizedMessage}")
+            null
+        }
+    }
+}
+
+//countTitlesPerCategory()
+/*
+
+fun countTitlesPerCategory() {
         val categoryCountMap = HashMap<String, Int>()
 
         mostPopularPagesRef.addListenerForSingleValueEvent(object : ValueEventListener {
@@ -121,254 +285,41 @@ class RandomArticleViewModel : ViewModel() {
         })
     }
 
-    suspend fun getRandomWikiEntryInCorrectLanguage(): String? {
-        // Get a random English article title
-        val title = getRandomWikiEntry()
 
-        if (lang == Language.ENGLISH.languageCode) {
-            return title
-        }
 
-        val translatedTitle = title?.let { translateTitleToLanguage(it, lang) }
-        return translatedTitle
-    }
 
-    suspend fun translateTitleToLanguage(title: String, languageCode: String): String? {
-        return withContext(Dispatchers.IO) {
-            val apiUrl = "https://en.wikipedia.org/w/api.php?action=query&prop=langlinks&titles=${title}&lllang=${languageCode}&format=json"
-            var translation = ""
 
-            try {
-                val url = URL(apiUrl)
-                val connection = url.openConnection() as HttpURLConnection
-                connection.requestMethod = "GET"
 
-                val reader = BufferedReader(InputStreamReader(connection.inputStream))
-                val response = reader.readText()
 
-                val json = JSONObject(response)
-                val query = json.optJSONObject("query")
-                val pages = query?.optJSONObject("pages")
 
-                val firstPageId = pages?.keys()?.next()
-                val page = pages?.optJSONObject(firstPageId)
 
-                val langLinks = page?.optJSONArray("langlinks")
-                langLinks?.let { links ->
-                    for (i in 0 until links.length()) {
-                        val langLink = links.optJSONObject(i)
-                        if (langLink.getString("lang") == languageCode) {
-                            translation = langLink.optString("*")
-                            return@let langLink.optString("*")
-                        }
-                    }
-                    null
-                }
-            } catch (e: Exception) {
-                e.printStackTrace()
-            }
 
-            return@withContext translation  // Return null if the translation is not found
-        }
-    }
 
-    suspend fun getRandomWikiEntry(): String? = suspendCoroutine { continuation ->
-        try {
+val mostPopularPagesWithCategoriesRef: DatabaseReference = FirebaseDatabase.getInstance().getReference("mostPopularPagesWithCategories")
 
-            //countTitlesPerCategory()
-            /*
-            val mostPopularPagesWithCategoriesRef: DatabaseReference = FirebaseDatabase.getInstance().getReference("mostPopularPagesWithCategories")
-
-            val uniqueCategories = mutableSetOf<String>()
+val uniqueCategories = mutableSetOf<String>()
 
 // Add a ValueEventListener to fetch and process the data
-            mostPopularPagesWithCategoriesRef.addValueEventListener(object : ValueEventListener {
-                override fun onDataChange(dataSnapshot: DataSnapshot) {
-                    // Iterate through the children of "mostPopularPagesWithCategories"
-                    for (childSnapshot in dataSnapshot.children) {
-                        // Get the "category" value from each child
-                        val category = childSnapshot.child("category").getValue(String::class.java)
-                        if (category != null) {
-                            uniqueCategories.add(category)
-                        }
-                    }
+mostPopularPagesWithCategoriesRef.addValueEventListener(object : ValueEventListener {
+    override fun onDataChange(dataSnapshot: DataSnapshot) {
+        // Iterate through the children of "mostPopularPagesWithCategories"
+        for (childSnapshot in dataSnapshot.children) {
+            // Get the "category" value from each child
+            val category = childSnapshot.child("category").getValue(String::class.java)
+            if (category != null) {
+                uniqueCategories.add(category)
+            }
+        }
 
-                    // Print the unique categories to the console
-                    for (category in uniqueCategories) {
-                        println(category)
-                    }
-                }
-
-                override fun onCancelled(databaseError: DatabaseError) {
-                    // Handle any errors here
-                }
-            })
-
-             */
-
-            val randomIndex = (0 until 8000).random()
-
-            // Form the reference to the random entry
-            val randomEntryRef = mostPopularPagesRef.child(randomIndex.toString())
-
-            // Fetch the random entry's title
-            randomEntryRef.addListenerForSingleValueEvent(object : ValueEventListener {
-                override fun onDataChange(dataSnapshot: DataSnapshot) {
-                    if (dataSnapshot.exists()) {
-                        // Get the random entry
-                        val title = dataSnapshot.child("title").value.toString()
-                        //Toast.makeText()
-                        continuation.resume(title)
-                    } else {
-                        // Handle the case where the random index doesn't exist
-                        continuation.resume(null)
-                    }
-                }
-
-                override fun onCancelled(databaseError: DatabaseError) {
-                    // Handle any errors during the query
-                    continuation.resume(null)
-                }
-            })
-        } catch (e: Exception) {
-            e.printStackTrace()
-            continuation.resume(null)
+        // Print the unique categories to the console
+        for (category in uniqueCategories) {
+            println(category)
         }
     }
 
-
-    suspend fun getRandomArticle(): String? = withContext(Dispatchers.IO) {
-        var urlConnection: HttpURLConnection? = null
-        try {
-            val url = URL("https://$lang.wikipedia.org/w/api.php?action=query&list=random&rnnamespace=0&format=json")
-            urlConnection = url.openConnection() as HttpURLConnection
-            val responseCode = urlConnection.responseCode
-
-            if (responseCode == HttpURLConnection.HTTP_OK) {
-                val reader = BufferedReader(InputStreamReader(urlConnection.inputStream))
-                val response = StringBuilder()
-
-                var line: String?
-                while (reader.readLine().also { line = it } != null) {
-                    response.append(line)
-                }
-                reader.close()
-
-                return@withContext parseRandomArticleTitle(response.toString())
-            }
-        } catch (e: Exception) {
-            e.printStackTrace()
-        } finally {
-            urlConnection?.disconnect()
-        }
-
-        return@withContext null
+    override fun onCancelled(databaseError: DatabaseError) {
+        // Handle any errors here
     }
-    suspend fun getRandomArticleFromCategory(category: String): String? = withContext(Dispatchers.IO) {
-        try {
-            var translatedCategory = category
-            if (lang != Language.ENGLISH.languageCode) {
-                translatedCategory = categoriesTranslation[category].toString()
-            }
-            val query = mostPopularPagesRef.orderByChild("category").equalTo(translatedCategory)
-            val dataSnapshot = query.get().await()
+})
 
-            if (dataSnapshot.exists()) {
-                val entries = mutableListOf<Map<String, Any>>()
-                for (childSnapshot in dataSnapshot.children) {
-                    val entry = childSnapshot.value as? Map<String, Any>
-                    entry?.let { entries.add(it) }
-                }
-
-                if (entries.isNotEmpty()) {
-                    val randomEntry = entries.random()
-                    val title = randomEntry["title"]?.toString()
-
-                    // If the language is English, return the title directly
-                    if (lang == Language.ENGLISH.languageCode) {
-                        return@withContext title
-                    } else {
-                        // If the language is not English, translate the title
-                        return@withContext title?.let { translateTitleToLanguage(it, lang) }
-                    }
-                }
-            }
-        } catch (e: Exception) {
-            e.printStackTrace()
-        }
-        return@withContext null
-    }
-
-    suspend fun getArticleByKeyword(keyword: String): String? = withContext(Dispatchers.IO) {
-        var urlConnection: HttpURLConnection? = null
-        try {
-            val encodedKeyword = URLEncoder.encode(keyword, "UTF-8")
-            val urlStr = "https://$lang.wikipedia.org/w/api.php?action=query&list=search&srsearch=$encodedKeyword&format=json"
-            val url = URL(urlStr)
-            urlConnection = url.openConnection() as HttpURLConnection
-            val responseCode = urlConnection.responseCode
-
-            if (responseCode == HttpURLConnection.HTTP_OK) {
-                val reader = BufferedReader(InputStreamReader(urlConnection.inputStream))
-                val response = StringBuilder()
-
-                var line: String?
-                while (reader.readLine().also { line = it } != null) {
-                    response.append(line)
-                }
-                reader.close()
-
-                return@withContext parseRandomArticleFromKeyword(response.toString())
-            }
-        } catch (e: Exception) {
-            e.printStackTrace()
-        } finally {
-            urlConnection?.disconnect()
-        }
-
-        return@withContext null
-    }
-
-    fun parseRandomArticleFromKeyword(json: String): String? {
-        val articleTitles = mutableListOf<String>()
-
-        try {
-            val jsonObject = JSONObject(json)
-            val queryObject = jsonObject.optJSONObject("query")
-            val searchArray = queryObject?.optJSONArray("search")
-
-            if (searchArray != null) {
-                for (i in 0 until searchArray.length()) {
-                    val item = searchArray.optJSONObject(i)
-                    val title = item?.optString("title")
-                    if (title != null) {
-                        articleTitles.add(title)
-                    }
-                }
-            }
-        } catch (e: JSONException) {
-            e.printStackTrace()
-        }
-
-        return if (articleTitles.isNotEmpty()) {
-            articleTitles.random()
-        } else {
-            null
-        }
-    }
-
-    private fun parseRandomArticleTitle(response: String): String? {
-        try {
-            val jsonObject = JSONObject(response)
-            val queryObject = jsonObject.getJSONObject("query")
-            val randomArray = queryObject.getJSONArray("random")
-            if (randomArray.length() > 0) {
-                val randomObject = randomArray.getJSONObject(0)
-                return randomObject.getString("title")
-            }
-        } catch (e: JSONException) {
-            e.printStackTrace()
-        }
-        return null
-    }
-}
+ */
